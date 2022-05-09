abd8760c316aead4645ca341a0321fe3c4be5dc5
Kristina Stasevich <3969626@gmail.com>
2022-05-09 14:53:58 +0300

    Last update for my sixth project

--- scripts/index.js
+++ scripts/index.js
@@ -86,46 +86,20 @@ function closeBigPicturePopup() {
 }
 
 function showInputError(formElement, inputElement, errorMessage) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
 
-    const errorElement = formElement.querySelector(`.${inputElement.id}__error`)
-
-    inputElement.classList.add("popup__input_type_error");
+    inputElement.classList.add("form__input_type_error");
     errorElement.textContent = errorMessage;
-    errorElement.classList.add("popup__input-error_active");
+    errorElement.classList.add("form__input-error_visible");
 }
 
 function hideInputError(formElement, inputElement) {
+    const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
 
-    const errorElement = formElement.querySelector(`.${inputElement.id}__error`)
-
-    inputElement.classList.remove("popup__input_type_error");
+    inputElement.classList.remove("form__input_type_error");
     errorElement.textContent = "";
-    errorElement.classList.remove("popup__input-error_active");
+    errorElement.classList.remove("form__input-error_visible");
 }
-
-
-const isValid = (formElement, inputElement) => {
-    console.log(inputElement.validity)
-    if (!inputElement.validity.valid) {
-        showInputError(formElement, inputElement, inputElement.validationMessage);
-    } else {
-        hideInputError(formElement, inputElement);
-    }
-};
-
-const setEventListeners = (formElement) => {
-
-    const inputList = Array.from(formElement.querySelectorAll(".popup__input"));
-
-
-    inputList.forEach((inputElement) => {
-
-        inputElement.addEventListener("input", () => {
-            isValid(formElement, inputElement)
-        });
-    });
-};
-
 
 document.addEventListener("keydown", function(evt) {
     const key = evt.key;
@@ -147,6 +121,42 @@ document.addEventListener("click", function(evt) {
         closePopup(bigPicturePopup);
     }
 });
+
+const isValid = (formElement, inputElement) => {
+    console.log(inputElement.validity);
+    if (!inputElement.validity.valid) {
+        showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+        hideInputError(formElement, inputElement);
+    }
+};
+
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid;
+    });
+};
+
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add("form__button_disabled");
+    } else {
+        buttonElement.classList.remove("form__button_disabled");
+    }
+};
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll(".form__input"));
+    const buttonElement = formElement.querySelector(".form__button");
+    //buttonElement.classList.add("popup__button_inactive");
+    toggleButtonState(inputList, buttonElement);
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener("input", () => {
+            isValid(formElement, inputElement);
+            toggleButtonState(inputList, buttonElement);
+        });
+    });
+};
 
 function handleProfileFormSubmit(evt) {
     evt.preventDefault();
@@ -228,14 +238,12 @@ for (const card of initialCards.reverse()) {
 }
 
 const enableValidation = () => {
-
     const formList = Array.from(document.querySelectorAll(".form"));
 
     formList.forEach((formElement) => {
         formElement.addEventListener("submit", (evt) => {
             evt.preventDefault();
         });
-
 
         setEventListeners(formElement);
     });
