a0af2e39548c85f3db729ea0c6d9e30f988d96d7
Kristina Stasevich <3969626@gmail.com>
2022-07-19 00:02:11 +0300

    update

--- src/components/UserInfo.js
+++ src/components/UserInfo.js
@@ -32,81 +32,35 @@ export default class UserInfo {
     }
 }
 
-// export default class Api {
-//     constructor({ baseUrl, headers }) {
-//         this._baseUrl = baseUrl;
-//         this._headers = headers;
+// import Popup from "./Popup";
+
+// export default class PopupWithConfirmation extends Popup {
+//     constructor(popupSelector) {
+//         super(popupSelector);
+//         this._form = this._popupElement.querySelector(".form");
+//         this._submitButton = this._form.querySelector("button");
+//         this._buttonText = this._submitButton.textContent;
 //     }
 
-//     _getResponseData(res) {
-//         if (!res.ok) {
-//             return Promise.reject(`Error: ${res.status}`);
+//     setAction(action) {
+//         this._handleSubmit = action;
+//     }
+
+//     setEventListeners() {
+//         this._form.addEventListener("submit", (evt) => {
+//             evt.preventDefault();
+//             this._handleSubmit();
+//             super.close();
+//         });
+
+//         super.setEventListeners();
+//     }
+
+//     renderLoading(isLoading) {
+//         if (isLoading) {
+//             this._submitButton.textContent = "Deleting...";
+//         } else {
+//             this._submitButton.textContent = this._buttonText;
 //         }
-//         return res.json();
-//     }
-
-//     getUserInfo() {
-//         return fetch(this._baseUrl + "/users/me", {
-//             headers: this._headers,
-//         }).then(this._getResponseData);
-//     }
-
-//     addUserAvatar(data) {
-//         return fetch(this._baseUrl + "/users/me/avatar", {
-//             method: "PATCH",
-//             headers: this._headers,
-//             body: JSON.stringify({
-//                 avatar: data.avatarLink,
-//             }),
-//         }).then(this._getResponseData);
-//     }
-
-//     addUserInfo(data) {
-//         return fetch(this._baseUrl + "/users/me", {
-//             method: "PATCH",
-//             headers: this._headers,
-//             body: JSON.stringify({
-//                 name: data.name,
-//                 about: data.job,
-//             }),
-//         }).then(this._getResponseData);
-//     }
-
-//     getCards() {
-//         return fetch(this._baseUrl + "/cards", {
-//             headers: this._headers,
-//         }).then(this._getResponseData);
-//     }
-
-//     addCard(data) {
-//         return fetch(this._baseUrl + "/cards", {
-//             method: "POST",
-//             headers: this._headers,
-//             body: JSON.stringify({
-//                 name: data.title,
-//                 link: data.image,
-//             }),
-//         }).then(this._getResponseData);
-//     }
-
-//     deleteCard(id) {
-//         return fetch(this._baseUrl + "/cards/" + id, {
-//             method: "DELETE",
-//             headers: this._headers,
-//         }).then(this._getResponseData);
-//     }
-
-//     addLikeCard(id) {
-//         return fetch(this._baseUrl + "/cards/likes/" + id, {
-//             method: "PUT",
-//             headers: this._headers,
-//         }).then(this._getResponseData);
-//     }
-
-//     removeLikeCard(id) {
-//         return fetch(this._baseUrl + "/cards/likes/" + id, {
-//             method: "DELETE",
-//             headers: this._headers,
-//         }).then(this._getResponseData);
 //     }
 // }
